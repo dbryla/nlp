1b2632c669e35139ee5c24a802e2236891efe6ad
Daniel Bryla <dbryla@student.agh.edu.pl>
2015-12-16 14:29:09 +0100

    Last changes

--- dict.py
+++ dict.py
@@ -2,18 +2,20 @@
 
 from lib.clp import clp
 
-# word = u'duży'
-# print word
-#
-# ids = clp(word)
-#
-# for id in ids:
-# 	print word, clp.label(id)[0], clp.vec(id, word)
+#word = u'duży'
+#print word
+
+#ids = clp(word)
+
+#for id in ids:
+#	print word, clp.label(id)[0], clp.vec(id, word)
 
 def check_if_verb(word):
-    ids = clp(word)
-    for id in ids:
-        if clp.vec(id, word) not in [1, 45, 46, 47, 13, 14]:
-            return clp.label(id)[0] == 'B'
-        else:
-            return False
+	#print word
+	ids = clp(word)
+	for id in ids:
+	#	print word, clp.vec(id, word)
+		if clp.vec(id, word) not in [1, 45, 46, 47, 13, 14] and word not in [u'był', 'jest', u'była']:
+			return clp.label(id)[0] == 'B'
+		else:
+			return False
